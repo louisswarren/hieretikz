709cae38850087dbac8fc86b9c5fbeb16ab3b8df
Louis Warren <louis@lsw.nz>
2017-05-01 18:17:16 +1200

    Implement other half of separation search

--- hieretikz.py
+++ hieretikz.py
@@ -21,39 +21,46 @@ class LongestPathDict(dict):
 
 
 @accumulate(LongestPathDict)
-def find_derivable(a, pf_adjacency, ignore=set()):
+def find_reachable(a, adjacency, ignore=set()):
+    """Returns a dictionary of all nodes reachable from a, mapped to the paths
+    taken to reach them."""
     yield a, ()
-    for b in pf_adjacency[a]:
+    for b in adjacency[a]:
         if b not in ignore:
             yield b, ((a, b),)
-            from_b = find_derivable(b, pf_adjacency, ignore | {a})
+            from_b = find_reachable(b, adjacency, ignore | {a})
             for c, path in from_b.items():
                 yield c, ((a,b),) + path
 
 # a |--- b : a -> ... -> b
 # a |-/- b : b -> ... -> c and a ||-/- c
 #         OR d -> ... -> a and d ||-/- b
-def find_relation(a, b, pf_adjacency, cm_adjacency):
-    """Returns pair (pp, spp), where pp is a path via proofs from a to b, and
-    spp is a path via proofs from b to some formula c, for which there is a
-    countermodel showing a does not imply c. Either pp or spp will be None If
-    both are none, the relation is unknown."""
-    print("\n\n\n", a, b)
-    a_b_path = find_derivable(a, pf_adjacency).get(b)
+def find_relation(a, b, pf_adjacency, pf_rev_adjacency, cm_adjacency):
+    """Returns pair (pp, spp), where at least one of pp and spp is None, and
+        - pp is a path via proofs from a to b
+        - spp is a path via proofs from either:
+            - b to some formula c, for which there is a countermodel showing a
+              does not imply c.
+            - some formula d to a, for which there is a countermodel showing d
+              does not imply b
+    If both pp and spp are none, the relation is unknown."""
+    a_b_path = find_reachable(a, pf_adjacency).get(b)
     if a_b_path:
         return a_b_path, None
-    b_consequences = find_derivable(b, pf_adjacency)
-    print(a, 'sep:', cm_adjacency[a])
+    b_consequences = find_reachable(b, pf_adjacency)
     for underivable in cm_adjacency[a]:
         if underivable in b_consequences:
             return None, b_consequences[underivable]
-    raise NotImplementedError("Need to check second condition")
+    a_causes = find_reachable(a, pf_rev_adjacency)
+    for cause in a_causes:
+        if b in cm_adjacency[cause]:
+            return None, find_reachable(cause, pf_adjacency)[a]
     return None, None
 
 @accumulate(set)
-def find_weak_edges(formulae, pf_adjacency, cm_adjacency):
+def find_weak_edges(formulae, pf_adjacency, pf_rev_adjacency, cm_adjacency):
     for a, b in itertools.permutations(formulae, 2):
-        pf, cm = find_relation(a, b, pf_adjacency, cm_adjacency)
+        pf, cm = find_relation(a, b, pf_adjacency, pf_rev_adjacency, cm_adjacency)
         if pf is None and cm is None:
             yield (a, b)
 
@@ -89,8 +96,9 @@ def make_tikz_edges(formulae, strong_edges, weak_edges):
 
 def make_tikz(formulae, formula_layout, proofs, counter_models):
     pf_adjacency = compute_adjacency(proofs)
+    pf_rev_adjacency = compute_adjacency((q, p) for p, q in proofs)
     cm_adjacency = compute_adjacency(counter_models)
-    weak_edges = find_weak_edges(formulae, pf_adjacency, cm_adjacency)
+    weak_edges = find_weak_edges(formulae, pf_adjacency, pf_rev_adjacency, cm_adjacency)
     tikz_nodes = make_tikz_nodes(formula_layout)
     tikz_edges = make_tikz_edges(formulae, proofs, weak_edges)
     return tikz_nodes + tikz_edges
