78d99638f831fac0a8ad8ae7ab04728336be8e9a
Louis Warren <louis@lsw.nz>
2017-05-01 15:36:25 +1200

    Use simpler test

--- test.py
+++ test.py
@@ -1,46 +1,25 @@
 from hieretikz import *
 from subprocess import Popen
 
-formulae = dne, lem, wlem, dp, he, dnsu, dnse, glpo, glpoa, gmp = \
-    'dne', 'lem', 'wlem', 'dp', 'he', 'dnsu', 'dnse', 'glpo', 'glpoa', 'gmp'
-
+formulae = dne, lem, lpo, wlem = 'dne lem lpo wlem'.split()
 
 _______ = None
 formula_layout = [
-[dne     , glpoa   , _______ , _______ , _______ , _______ , _______ ],
-[_______ , _______ , _______ , lem     , _______ , glpo    , _______ ],
-[_______ , _______ , _______ , _______ , _______ , _______ , _______ ],
-[_______ , dp      , _______ , _______ , _______ , he      , _______ ],
-[dnsu    , _______ , gmp     , _______ , _______ , _______ , dnse    ],
-[_______ , _______ , _______ , wlem    , _______ , _______ , _______ ],
+[_______ , dne     , _______ ],
+[lem     , _______ , lpo     ],
+[_______ , wlem    , _______ ],
 ]
 
 proofs = {
-        (dne, glpoa): 'pft',
-        (lem, wlem):  'pf0',
-        (dp, wlem):   'pf1',
-        (he, wlem):   'pf2',
-        (glpoa, lem): 'pf3',
-        (lem, glpo):  'pf4',
-        (glpo, lem):  'pf5',
-        (dp, gmp):    'pf6',
-        (gmp, wlem):  'pf7',
-        (dp, dnsu):   'pf8',
-        (he, dnse):   'pf9',
+        (dne, lem):  'pf0',
+        (lem, lpo):  'pf1',
+        (lpo, lem):  'pf1',
+        (lem, wlem): 'pf2',
         }
 
 counter_models = {
-        (glpoa, dne): 'cmt',
-        (wlem, lem):  'cm0',
-        (wlem, dp):   'cm1',
-        (wlem, he):   'cm2',
-        (lem, glpoa): 'cm3',
-        (glpo, lem):  'cm4',
-        (lem, glpo):  'cm5',
-        (gmp, dp):    'cm6',
-        (wlem, gmp):  'cm7',
-        (dnsu, dp):   'cm8',
-        (dnse, he):   'cm9',
+        (lem, dne):  'cm0',
+        (wlem, lem): 'cm1',
         }
 
 
