9245dc994735e7060414eaa4b3df1b6f2447414f
Louis Warren <louis@lsw.nz>
2017-05-03 16:14:16 +1200

    Add some docstrings

--- hierarchy.py
+++ hierarchy.py
@@ -1,25 +1,29 @@
-"""Reason about a directed graph in which the (non-)existance of some edges
-must be inferred by the disconnectedness of certain vertices"""
+'''Reason about a directed graph in which the (non-)existance of some edges
+must be inferred by the disconnectedness of certain vertices'''
 
 def transitive_closure_set(vertices, edges):
+    '''Find the transitive closure of a set of vertices.'''
     neighbours = {b for a, b in edges if a in vertices}
     if neighbours.issubset(vertices):
         return vertices
     return transitive_closure_set(vertices | neighbours, edges)
 
 def downward_closure(vertex, edges):
+    '''Find the downward closure of a vertex.'''
     return transitive_closure_set({vertex}, edges)
 
 def upward_closure(vertex, edges):
+    '''Find the upward closure of a vertex.'''
     return transitive_closure_set({vertex}, {(b, a) for a, b in edges})
 
 def is_connected(a, b, edges):
+    '''Check if there is a path from a to b.'''
     return b in downward_closure(a, edges)
 
 def is_separated(a, b, edges, disconnections, a_checked=None, b_checked=None):
-    """Checks that a and b will remain not connected, even if edges are added
-    to the graph, as long as the vertex pairs listed in disconnections remain
-    disconected."""
+    '''Checks that a and b will remain not connected even if edges are added to
+    the graph, as long as the vertex pairs listed in disconnections remain
+    disconected.'''
     if (a, b) in disconnections:
         return True
     a_checked = a_checked or set()
@@ -35,8 +39,8 @@ def is_separated(a, b, edges, disconnections, a_checked=None, b_checked=None):
     return False
 
 def find_possible_connections(vertices, edges, disconnections):
-    """Find which edges can be added to create new connections, without
-    connecting any pairs in disconnections."""
+    '''Find which edges can be added to create new connections, without
+    connecting any pairs in disconnections.'''
     return {(a, b) for a in vertices for b in vertices if
             not is_connected(a, b, edges) and
             not is_separated(a, b, edges, disconnections)}
