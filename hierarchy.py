6e2a55b8270f507d5fdff1df731f4310b3174f3f
Louis Warren <louis@lsw.nz>
2017-05-03 14:58:01 +1200

    Move testing out

--- hierarchy.py
+++ hierarchy.py
@@ -1,33 +1,3 @@
-compose = lambda f: lambda g: lambda *a, **k: f(g(*a, **k))
-trace = lambda f: lambda *a: print('{}({}) -> {}'.format(f.__name__, ', '.join(map(repr, a)), repr(f(*a)))) or f(*a)
-
-va, vb, vp, vq, vr, vx, vy, vz = 'a b p q r x y z'.split()
-'''
-    vp -- > vq
-     |      |
-     v      v
-    va <--> vr
-     ?
-     ?
-     ?
-    vb --> vx
-     |      |
-     v      v
-    vy --> vz
-'''
-edges = {
-    (vp, va),
-    (vp, vq),
-    (vq, vr),
-    (vr, va),
-    (va, vr),
-    (vb, vx),
-    (vb, vy),
-    (vx, vz),
-    (vy, vz),
-}
-
-
 def transitive_closure_set(vertices, edges):
     neighbours = {b for a, b in edges if a in vertices}
     if neighbours.issubset(vertices):
@@ -44,6 +14,9 @@ def is_connected(a, b, edges):
     return b in downward_closure(a, edges)
 
 def is_separated(a, b, edges, disconnections):
+    """Checks that a and b will remain not connected, even if edges are added
+    to the graph, as long as the vertex pairs listed in disconnections remain
+    disconected."""
     if (a, b) in disconnections:
         return True
     # Removing the alternate closures removes the relevant vertex, and avoids
@@ -55,37 +28,3 @@ def is_separated(a, b, edges, disconnections):
             if is_separated(p, q, edges, disconnections):
                 return True
     return False
-
-def test(f, *args, **kwargs):
-    args_str = ', '.join(map(repr, args))
-    kwargs_str = ', '.join('{}={}'.format(k, repr(kwargs[k])) for k in kwargs)
-    full_args_str = ', '.join(s for s in (args_str, kwargs_str) if s)
-    def inner(expected):
-        result = f(*args, **kwargs)
-        test_str = "{}({}) -> {}".format(f.__name__, full_args_str, result)
-        if result == expected:
-            print("Pass:", test_str)
-        else:
-            print("FAIL:", test_str)
-        return not result == expected
-    return inner
-
-def test_all():
-    class _quiet_set(set):
-        __repr__ = lambda s: 'quiet'
-    qedges = _quiet_set(edges)
-
-    yield test(downward_closure, vb, qedges)({vb, vx, vy, vz})
-    yield test(is_connected, vp, va, qedges)(True)
-    yield test(is_connected, vx, vb, qedges)(False)
-    yield test(is_separated, va, vb, qedges, {(vq, vz)} )(True)
-    yield test(is_separated, va, vb, qedges, {(vq, va)} )(False)
-
-def run_tests():
-    failures = sum(test_all())
-    if failures:
-        print()
-        print("{} test cases FAILED".format(failures))
-
-if __name__ == '__main__':
-    run_tests()
--- test.py
+++ test.py
@@ -1,50 +1,64 @@
-from hieretikz import *
-import subprocess
+from hierarchy import *
 
-formulae = dne, lem, lpo, wlem, mwlpo = 'dne lem lpo wlem mwlpo'.split()
+compose = lambda f: lambda g: lambda *a, **k: f(g(*a, **k))
+trace = lambda f: lambda *a: print('{}({}) -> {}'.format(f.__name__, ', '.join(map(repr, a)), repr(f(*a)))) or f(*a)
 
-_______ = None
-formula_layout = [
-'          dne               ',
-'lem                 lpo     ',
-'         wlem               ',
-'         mwlpo              ',
-'                            ',
-]
-
-proofs = {
-        (dne, lem):    'pf0',
-        (lem, lpo):    'pf1',
-        (lpo, lem):    'pf1',
-        (lem, wlem):   'pf2',
-        (wlem, mwlpo): 'pf2',
-        }
-
-counter_models = {
-        (lem, dne):  'cm0',
-        (wlem, lem): 'cm1',
-        }
-
-document = r'''
-\documentclass{article}
-\usepackage{tikz}
-\begin{document}
-\begin{tikzpicture}[node distance=2 cm, line width=0.3mm, auto]
-''' + \
-make_tikz(formulae, formula_layout, proofs, counter_models) + \
-r'''
-\end{tikzpicture}
-\end{document}
+va, vb, vp, vq, vr, vx, vy, vz = 'a b p q r x y z'.split()
 '''
-with open('test.tex', 'w') as f:
-    f.write(document)
-subprocess.Popen(['pdflatex', 'test.tex'], stdout=subprocess.DEVNULL)
+    vp -- > vq
+     |      |
+     v      v
+    va <--> vr
+     ?
+     ?
+     ?
+    vb --> vx
+     |      |
+     v      v
+    vy --> vz
+'''
+edges = {
+    (vp, va),
+    (vp, vq),
+    (vq, vr),
+    (vr, va),
+    (va, vr),
+    (vb, vx),
+    (vb, vy),
+    (vx, vz),
+    (vy, vz),
+}
 
+def test(f, *args, **kwargs):
+    args_str = ', '.join(map(repr, args))
+    kwargs_str = ', '.join('{}={}'.format(k, repr(kwargs[k])) for k in kwargs)
+    full_args_str = ', '.join(s for s in (args_str, kwargs_str) if s)
+    def inner(expected):
+        result = f(*args, **kwargs)
+        test_str = "{}({}) -> {}".format(f.__name__, full_args_str, result)
+        if result == expected:
+            print("Pass:", test_str)
+        else:
+            print("FAIL:", test_str)
+        return not result == expected
+    return inner
 
+def test_all():
+    class _quiet_set(set):
+        __repr__ = lambda s: 'quiet'
+    qedges = _quiet_set(edges)
 
-# Debugging
+    yield test(downward_closure, vb, qedges)({vb, vx, vy, vz})
+    yield test(is_connected, vp, va, qedges)(True)
+    yield test(is_connected, vx, vb, qedges)(False)
+    yield test(is_separated, va, vb, qedges, {(vq, vz)} )(True)
+    yield test(is_separated, va, vb, qedges, {(vq, va)} )(False)
 
-# print('='*80)
-# pf_adjacency = compute_adjacency(proofs)
-# cm_adjacency = compute_adjacency(counter_models)
-# print(find_relation(lpo, dne, pf_adjacency, cm_adjacency))
+def run_tests():
+    failures = sum(test_all())
+    if failures:
+        print()
+        print("{} test cases FAILED".format(failures))
+
+if __name__ == '__main__':
+    run_tests()
