af930e205072b732f2ab3c020c4b6fc0a07b1980
Louis Warren <louis@lsw.nz>
2017-05-03 18:49:58 +1200

    Vastly simplify is_separated function

--- hierarchy.py
+++ hierarchy.py
@@ -20,23 +20,13 @@ def is_connected(a, b, edges):
     '''Check if there is a path from a to b.'''
     return b in downward_closure(a, edges)
 
-def is_separated(a, b, edges, disconnections, a_checked=None, b_checked=None):
-    '''Checks that a and b will remain not connected even if edges are added to
+def is_separated(a, b, edges, disconnections):
+    '''Check that a and b will remain not connected even if edges are added to
     the graph, as long as the vertex pairs listed in disconnections remain
     disconected.'''
-    if (a, b) in disconnections:
-        return True
-    a_checked = a_checked or set()
-    b_checked = b_checked or set()
-    above_a = upward_closure(a, edges) - a_checked
-    below_b = downward_closure(b, edges) - b_checked
-    new_a_checked = a_checked | {a}
-    new_b_checked = b_checked | {b}
-    for p in above_a:
-        for q in below_b:
-            if is_separated(p, q, edges, disconnections, new_a_checked, new_b_checked):
-                return True
-    return False
+    return any((p, q) in disconnections
+               for p in upward_closure(a, edges)
+               for q in downward_closure(b, edges))
 
 def find_possible_connections(vertices, edges, disconnections):
     '''Find which edges can be added to create new connections, without
