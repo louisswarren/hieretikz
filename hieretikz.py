41f74a2a2c1365c2b5942a742e10a5141b5e0550
Louis Warren <louis@lsw.nz>
2017-05-10 12:19:34 +1200

    Add docstrings

--- hieretikz.py
+++ hieretikz.py
@@ -3,12 +3,14 @@ from constructive_hierarchy import *
 _compose = lambda f: lambda g: lambda *a, **k: f(g(*a, **k))
 
 def all_separations(models):
+    '''Find all logical separations indicated by a set of models.'''
     return {(holds, fails): cm for cm, fpair in models.items()
                                for holds in fpair[0]
                                for fails in fpair[1]}
 
 @_compose('\n'.join)
 def string_node_layout_to_tikz(formula_layout):
+    '''Convert string diagram into tikz nodes.'''
     formulae = formula_layout.split()
     fmt = r'\node ({}) at ({}, {}) {{{}}};'
     for row_num, row in enumerate(formula_layout.split('\n')):
@@ -22,6 +24,7 @@ def string_node_layout_to_tikz(formula_layout):
                 col_num += len(formula)
 
 def _generate_tikz_edges(edge_set, arrow_type='', avoid_overlap=()):
+    '''Generate tiks arrows for a set of directed edges.'''
     if arrow_type:
         fmt = r'\draw[' + arrow_type + r', {}] ({}) to node {{}} ({});'
     else:
@@ -40,18 +43,21 @@ def _generate_tikz_edges(edge_set, arrow_type='', avoid_overlap=()):
 
 @_compose('\n'.join)
 def make_tikz_edges(strong_edges, weak_edges):
+    '''Generate the strong and weak tikz arrows for a tikz diagram.'''
     yield from _generate_tikz_edges(spanning_tree(set(strong_edges)))
     yield from _generate_tikz_edges(weak_edges, 'dashed', strong_edges)
 
 @_compose('\n'.join)
 def make_tikz_diagram(formula_layout, strong_edges, weak_edges):
+    '''Generate a tikz diagram.'''
     yield r'\begin{tikzpicture}[line width=0.3mm, auto]'
     yield string_node_layout_to_tikz(formula_layout)
     yield make_tikz_edges(strong_edges, weak_edges)
     yield r'\end{tikzpicture}'
 
 @_compose('\n'.join)
-def make_tikz_questions(evaluated_weak_edges):
+def make_questions(evaluated_weak_edges):
+    '''Generate latex output for open questions.'''
     yield r'\begin{multicols}{3} \noindent'
     order = lambda x: (-min(x[1]), x[0])
     for edge, rank in sorted(evaluated_weak_edges.items(), key=order):
@@ -60,6 +66,7 @@ def make_tikz_questions(evaluated_weak_edges):
 
 @_compose('\n'.join)
 def hieretikz(formulae, formula_layout, proofs, models):
+    '''Get hieretikz output.'''
     evaluated_weak_edges = find_evaluated_connections(
             formulae, set(proofs), set(all_separations(models)))
     drawable_nodes = formula_layout.split()
@@ -71,10 +78,11 @@ def hieretikz(formulae, formula_layout, proofs, models):
     if evaluated_weak_edges:
         yield r'\paragraph{}'
         yield r'It remains to investigate:'
-        yield make_tikz_questions(evaluated_weak_edges)
+        yield make_questions(evaluated_weak_edges)
 
 @_compose('\n'.join)
 def hieretikz_document_wrap(tex):
+    '''Generate the wrapping needed for making a complete hieretikz document.'''
     yield r'\documentclass{article}'
     yield r'\usepackage{tikz}'
     yield r'\usepackage{amsmath}'
@@ -86,5 +94,6 @@ def hieretikz_document_wrap(tex):
 
 @_compose('\n'.join)
 def hieretikz_output_document(formulae, formula_layout, proofs, models):
+    '''Get hieretikz output as a full document.'''
     tex = hieretikz(formulae, formula_layout, proofs, models)
     yield hieretikz_document_wrap(tex)
