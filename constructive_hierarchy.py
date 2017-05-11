adc58406afd778646a4d6abeec100955b8ee9af4
Louis Warren <louis@lsw.nz>
2017-05-11 16:58:35 +1200

    Remove 84601 cacheable function calls

--- constructive_hierarchy.py
+++ constructive_hierarchy.py
@@ -1,6 +1,26 @@
 '''Reason about a directed graph in which the (non-)existence of some edges
 must be inferred by the disconnectedness of certain vertices.'''
 
+import functools
+
+_cache_memory = {}
+def _cache(f):
+    @functools.wraps(f)
+    def inner(*args):
+        frozen_args = []
+        for arg in args:
+            if type(arg) is set:
+                frozen_args.append(frozenset(arg))
+            elif type(arg) is list:
+                frozen_args.append(tuple(arg))
+            else:
+                frozen_args.append(arg)
+        key = (f, tuple(frozen_args))
+        if key not in _cache_memory:
+            _cache_memory[key] = f(*frozen_args)
+        return _cache_memory[key]
+    return inner
+
 def transitive_closure_dict(known_vertices, edges):
     '''Find the transitive closure of a dict mapping vertices to their paths.'''
     found_vertices = {b: known_vertices[a] + ((a, b),)
@@ -10,6 +30,7 @@ def transitive_closure_dict(known_vertices, edges):
     found_vertices.update(known_vertices)
     return transitive_closure_dict(found_vertices, edges)
 
+@_cache
 def transitive_closure(vertex, edges):
     '''Find the transitive closure of a vertex.'''
     closure = transitive_closure_dict({vertex: ()}, edges)
@@ -17,24 +38,29 @@ def transitive_closure(vertex, edges):
     closure[vertex] = (vertex, vertex),
     return closure
 
+@_cache
 def downward_closure(vertex, edges):
     '''Find the downward closure of a vertex.'''
     return transitive_closure(vertex, edges)
 
+@_cache
 def _reverse_edges(edges):
     if isinstance(edges, dict):
         return {(b, a): edges[(a, b)] for a, b in edges}
     return type(edges)((b, a) for a, b in edges)
 
+@_cache
 def upward_closure(vertex, edges):
     '''Find the upward closure of a vertex.'''
     reversed_paths = transitive_closure(vertex, _reverse_edges(edges))
     return {v: _reverse_edges(p) for v, p in reversed_paths.items()}
 
+@_cache
 def is_connected(a, b, edges):
     '''Find a (truthy) path from a to b if one exists.'''
     return downward_closure(a, edges).get(b, False)
 
+@_cache
 def is_separated(a, b, edges, disconnections):
     '''Find a (truthy) reason why a and b are separated, if one exists.
 
@@ -49,6 +75,7 @@ def is_separated(a, b, edges, disconnections):
                 return path_from_p_to_a, path_from_b_to_q
     return False
 
+@_cache
 def find_possible_connections(vertices, edges, disconnections):
     '''Find all new consistent connecting edges.
 
@@ -58,6 +85,7 @@ def find_possible_connections(vertices, edges, disconnections):
             if not is_connected(a, b, edges)
             if not is_separated(a, b, edges, disconnections)}
 
+@_cache
 def is_redundant_edge(edge, edges):
     '''Give alternate path if one exists.'''
     return is_connected(*edge, edges - {edge})
@@ -69,6 +97,7 @@ def spanning_tree(edges):
             return spanning_tree(edges - {edge})
     return edges
 
+@_cache
 def evaluate_possible_edge(edge, vertices, edges, disconnections):
     '''Find the value of knowing about the existence of an edge.
 
@@ -80,6 +109,7 @@ def evaluate_possible_edge(edge, vertices, edges, disconnections):
     not_exists_learned = unknown - evaluator(edges, disconnections | {edge})
     return exists_learned, not_exists_learned
 
+@_cache
 def find_evaluated_connections(vertices, edges, disconnections):
     '''Find all new consistent connecting edges and their evaluations.
 
